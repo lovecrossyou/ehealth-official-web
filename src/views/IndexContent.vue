e855b488b5f2ca6b672c76489f022d394c1a7645
love1234 <345841634@qq.com>
2019-05-27 18:05:03 +0800

    xx

--- src/views/IndexContent.vue
+++ src/views/IndexContent.vue
@@ -1,81 +1,156 @@
 <template>
   <div class="main_content">
     <div class="index_content">
-      <!-- 左侧面板 ------------------------------->
-      <div class="content_left">
-        <!-- 媒体聚焦 -->
-        <div>
-          <NaviTitle title="媒体聚焦"></NaviTitle>
-        </div>
+      <div>
+        <div class="main-left">
+          <!-- 左侧面板 ------------------------------->
+          <div class="content_left">
+            <!-- 媒体聚焦 -->
+            <div>
+              <NaviTitle title="媒体聚焦"></NaviTitle>
+            </div>
 
-        <div class="meiti-items">
-          <div href="/cxcyds/jdxw/201905/ded81ead002d442d9049064693551016.shtml" target="_blank">
-            <div class="gg1 gg">
-              <img
-                src="http://www.cxcyds.com/cxcyds/jdxw//201905/ded81ead002d442d9049064693551016/images/9c9b727727754478a843d7e8919bf0b7.png"
+            <div class="meiti-items">
+              <div
+                href="/cxcyds/jdxw/201905/ded81ead002d442d9049064693551016.shtml"
+                target="_blank"
               >
-              <div class="meiti-item-desc">2019第八届中国创新创业大赛国际第 三代半导体专业正式启动</div>
+                <div class="gg1 gg">
+                  <img
+                    src="http://www.cxcyds.com/cxcyds/jdxw//201905/ded81ead002d442d9049064693551016/images/9c9b727727754478a843d7e8919bf0b7.png"
+                  >
+                  <div class="meiti-item-desc">2019第八届中国创新创业大赛国际第 三代半导体专业正式启动</div>
+                </div>
+              </div>
+              <div
+                href="/cxcyds/jdxw/201905/ded81ead002d442d9049064693551016.shtml"
+                target="_blank"
+              >
+                <div class="gg1 gg">
+                  <img
+                    src="http://www.cxcyds.com/cxcyds/jdxw//201905/ded81ead002d442d9049064693551016/images/9c9b727727754478a843d7e8919bf0b7.png"
+                  >
+                  <div class="meiti-item-desc">2019第八届中国创新创业大赛国际第 三代半导体专业正式启动</div>
+                </div>
+              </div>
+            </div>
+
+            <div style="padding:20px 0 15px 0;">
+              <NaviTitle title="大赛公告"></NaviTitle>
+            </div>
+            <div>
+              <el-steps direction="vertical">
+                <el-step title="科技部关于举办第八届中国创新创业大赛的通知  " icon="el-icon-loading"></el-step>
+                <el-step title="科技部火炬中心关于组织第七届中国创新创业大赛优秀企业" icon="el-icon-loading"></el-step>
+                <el-step title="关于对第七届中国创新创业大赛优秀组织单位进行表彰的通知  " description icon="el-icon-loading"></el-step>
+                <el-step title="第七届中国创新创业大赛新材料行业总决赛获奖及优秀企业名单公示" icon="el-icon-loading"></el-step>
+                <el-step title="第七届中国创新创业大赛生物医药行业总决赛获奖及优秀企业名单公示" icon="el-icon-loading"></el-step>
+                <el-step title="第七届中国创新创业大赛生物医药行业总决赛获奖及优秀企业名单公示" icon="el-icon-loading"></el-step>
+              </el-steps>
+            </div>
+
+            <!-- 全国行业总决赛 专业赛事-->
+            <div class="quanguo-game-wrapper">
+              <div class="quanguo-game-item">生物医药</div>
+              <div class="quanguo-game-item">电子信息</div>
+              <div class="quanguo-game-item">新能源及节能环保</div>
+              <div class="quanguo-game-item">互联网</div>
+              <div class="quanguo-game-item">新材料</div>
+              <div class="quanguo-game-item">先进制造</div>
+            </div>
+
+            <!-- 大企业需求对接 -->
+            <div style="padding-top:20px;">
+              <NaviTitle title="大企业需求对接"></NaviTitle>
+            </div>
+
+            <div class="big-business">
+              <div class="big-business-item"></div>
+              <div class="big-business-item"></div>
+              <div class="big-business-item"></div>
+              <div class="big-business-item"></div>
             </div>
           </div>
-          <div href="/cxcyds/jdxw/201905/ded81ead002d442d9049064693551016.shtml" target="_blank">
-            <div class="gg1 gg">
-              <img
-                src="http://www.cxcyds.com/cxcyds/jdxw//201905/ded81ead002d442d9049064693551016/images/9c9b727727754478a843d7e8919bf0b7.png"
-              >
-              <div class="meiti-item-desc">2019第八届中国创新创业大赛国际第 三代半导体专业正式启动</div>
+          <!-- 中间内容 ------------------------------->
+          <div class="center_content">
+            <!-- 大赛新闻 -->
+            <NaviTitle title="大赛新闻"></NaviTitle>
+            <div class="top-news">
+              <div class="top-news-image"></div>
+              <div style="padding-left:13px;">
+                <div class="news-title">江阴|2019年中国江阴（高新区）创新 创业大赛总决赛即将</div>
+                <div class="news-desc">以特钢新材料及制品、微电子集成电路、现代中药和生物医药、机械智能制造等高层次产.....</div>
+                <div class="news-footer">
+                  <div class="news-footer-source">来源：江阴高新区</div>
+                  <div class="news-footer-time">2019-05-22</div>
+                </div>
+              </div>
+            </div>
+            <div class="short-news">
+              <div class="short-news-item">广西|第八届中国创新创业大赛广西赛区暨2019年广西创新创业大赛正式启动</div>
+              <div class="short-news-item">第八届中国创新创业大赛江苏赛区</div>
+              <div class="short-news-item">广州|第八届中国创新创业大赛（广东﹒广州赛区）正式启航，大湾区科技创新服....</div>
+              <div class="short-news-item">大连|吹响创新创业创投集结号，第八届中国创新创业大赛（大连赛区）暨第......</div>
+              <div class="short-news-item">河北|第八届中国创新创业大赛（河北赛区）暨第七届河北省创新创业大赛正式启动</div>
+            </div>
+            <!-- 地方赛动态 -->
+            <div style="padding-top:20px;">
+              <NaviTitle title="地方赛动态"></NaviTitle>
+            </div>
+            <div class="top-news">
+              <div class="top-news-image"></div>
+              <div style="padding-left:13px;">
+                <div class="news-title">江阴|2019年中国江阴（高新区）创新 创业大赛总决赛即将</div>
+                <div class="news-desc">以特钢新材料及制品、微电子集成电路、现代中药和生物医药、机械智能制造等高层次产.....</div>
+                <div class="news-footer">
+                  <div class="news-footer-source">来源：江阴高新区</div>
+                  <div class="news-footer-time">2019-05-22</div>
+                </div>
+              </div>
+            </div>
+            <div class="short-news">
+              <div class="short-news-item">广西|第八届中国创新创业大赛广西赛区暨2019年广西创新创业大赛正式启动</div>
+            </div>
+
+            <!-- 赛友捷报 -->
+            <div style="padding-top:20px;">
+              <NaviTitle title="赛友捷报"></NaviTitle>
+            </div>
+            <div class="top-news">
+              <div class="top-news-image"></div>
+              <div style="padding-left:13px;">
+                <div class="news-title">江阴|2019年中国江阴（高新区）创新 创业大赛总决赛即将</div>
+                <div class="news-desc">以特钢新材料及制品、微电子集成电路、现代中药和生物医药、机械智能制造等高层次产.....</div>
+                <div class="news-footer">
+                  <div class="news-footer-source">来源：江阴高新区</div>
+                  <div class="news-footer-time">2019-05-22</div>
+                </div>
+              </div>
+            </div>
+            <div class="short-news">
+              <div class="short-news-item">广西|第八届中国创新创业大赛广西赛区暨2019年广西创新创业大赛正式启动</div>
             </div>
           </div>
         </div>
-
-        <div style="padding:20px 0 15px 0;">
-          <NaviTitle title="大赛公告"></NaviTitle>
-        </div>
-        <div>
-          <el-steps direction="vertical">
-            <el-step title="科技部关于举办第八届中国创新创业大赛的通知  " icon="el-icon-loading"></el-step>
-            <el-step title="科技部火炬中心关于组织第七届中国创新创业大赛优秀企业" icon="el-icon-loading"></el-step>
-            <el-step title="关于对第七届中国创新创业大赛优秀组织单位进行表彰的通知  " description icon="el-icon-loading"></el-step>
-            <el-step title="第七届中国创新创业大赛新材料行业总决赛获奖及优秀企业名单公示" icon="el-icon-loading"></el-step>
-            <el-step title="第七届中国创新创业大赛生物医药行业总决赛获奖及优秀企业名单公示" icon="el-icon-loading"></el-step>
-            <el-step title="第七届中国创新创业大赛生物医药行业总决赛获奖及优秀企业名单公示" icon="el-icon-loading"></el-step>
-          </el-steps>
-        </div>
-
-        <!-- 全国行业总决赛 专业赛事-->
-        <div class="quanguo-game-wrapper">
-          <div class="quanguo-game-item">生物医药</div>
-          <div class="quanguo-game-item">电子信息</div>
-          <div class="quanguo-game-item">新能源及节能环保</div>
-          <div class="quanguo-game-item">互联网</div>
-          <div class="quanguo-game-item">新材料</div>
-          <div class="quanguo-game-item">先进制造</div>
-        </div>
-
-        <!-- 大企业需求对接 -->
         <div style="padding-top:20px;">
-          <NaviTitle title="大企业需求对接"></NaviTitle>
+          <NaviTitle title="项目直通车 "></NaviTitle>
         </div>
-
-        <div class="big-business">
-          <div class="big-business-item"></div>
-          <div class="big-business-item"></div>
-          <div class="big-business-item"></div>
-          <div class="big-business-item"></div>
-        </div>
-      </div>
-
-      <!-- 中间内容 ------------------------------->
-      <div class="center_content">
-        <NaviTitle title="大赛新闻"></NaviTitle>
-        <div class="top-news">
-          <div class="top-news-image"></div>
-          <div style="padding-left:13px;">
-            <div class="news-title">江阴|2019年中国江阴（高新区）创新 创业大赛总决赛即将</div>
-            <div class="news-desc">以特钢新材料及制品、微电子集成电路、现代中药和生物医药、机械智能制造等高层次产.....</div>
-            <div class="news-footer">
-              <div class="news-footer-source">来源：江阴高新区</div>
-              <div class="news-footer-time">2019-05-22</div>
-            </div>
+        <div class="project-items">
+          <div class="project-item">
+            <img class="project-item-logo" src="http://qnimage.xiteng.com/p_7@2x.png"/>
+            <div class="project-item-title">“花开花落”创意有限责任公司</div>
+          </div>
+          <div class="project-item">
+            <img class="project-item-logo" src="http://qnimage.xiteng.com/p_7@2x.png"/>
+            <div class="project-item-title">第三创意人力资源有限公司</div>
+          </div>
+          <div class="project-item">
+            <img class="project-item-logo" src="http://qnimage.xiteng.com/p_7@2x.png"/>
+            <div class="project-item-title">”濠江映画“特色手工画舫商 业计划书</div>
+          </div>
+          <div class="project-item">
+            <img class="project-item-logo" src="http://qnimage.xiteng.com/p_7@2x.png"/>
+            <div class="project-item-title">”濠江映画“特色手工画舫商 业计划书</div>
           </div>
         </div>
       </div>
@@ -127,6 +202,12 @@ export default {
   display: flex;
   flex-direction: row;
   width: 1048px;
+  height: 845px;
+}
+
+.main-left {
+  display: flex;
+  flex-direction: row;
 }
 
 .content_left {
@@ -245,6 +326,49 @@ height:30px; */
   /* background-color: aqua; */
   /* height: 300px; */
   padding: 0 30px;
+}
+
+.project-items {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+
+.project-item {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 195px;
+  height: 192px;
+  margin-right: 14px;
+}
+
+.project-item-logo {
+  width: 194px;
+  height: 138px;
+}
+.project-item-title {
+  font-size: 12px;
+  font-family: MicrosoftYaHei;
+  font-weight: 400;
+  color: rgba(31, 31, 31, 1);
+  padding-top: 19px;
+  text-align: center;
+}
+
+.short-news {
+  margin-top: 20px;
+}
+
+.short-news-item {
+  /* width: 391px;
+  height: 107px; */
+  font-size: 11px;
+  font-family: SimSun;
+  font-weight: 400;
+  color: rgba(50, 50, 50, 1);
+  /* line-height: 24px; */
 }
 
 .top-news {
